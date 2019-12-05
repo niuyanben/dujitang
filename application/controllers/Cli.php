89272a937acb2541eaf8e72db8f7ba0580e0e1a5
meiwenhui <meiwenhui@163.com>
2019-12-05 23:31:21 +0800

    redis list to set

--- application/controllers/Cli.php
+++ application/controllers/Cli.php
@@ -14,15 +14,23 @@ class Cli extends Base_Controller {
 	public function redis() {
 
 		$this->load->model('archive_model');
-		$archiveList = $this->archive_model->random(5);
-		// 
+		$archiveCount = $this->archive_model->count();
+
+		$limit = 20;
+		$pages = ceil($archiveCount/$limit);
+
+		echo "共 {$archiveCount} 条记录，每页 {$limit} 条，分 {$pages} 页..." . PHP_EOL;
 		$this->load->driver('cache', ['adapter' => 'redis']);
-
-
-		foreach($archiveList as $archive){
-			$this->cache->redis->lpush('archives', json_encode($archive, JSON_UNESCAPED_UNICODE));
-		}
+		$this->cache->redis->delete('archives'); // 
 		
+		for ($i=0; $i < $pages; $i++) { 
+			$offset = $i * $limit;
+			echo "  开始导第 {$i} 页， 从 {$offset} 开始" . PHP_EOL;
+			$archiveList = $this->archive_model->select(null, null, 'id asc', $limit, $offset);
+			foreach($archiveList as $archive){
+				$this->cache->redis->lpush('archives', json_encode($archive, JSON_UNESCAPED_UNICODE)); // 
+			}
+		}
 		echo 'DONE' . PHP_EOL;
 	}
 
